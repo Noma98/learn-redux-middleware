a5712f16228e05fe93a5503002ecd101b9283cc4
Noma <wiostz98kr@gmail.com>
2021-09-30 23:22:26 +0900

    Make counter module use redux-saga

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import createSagaMiddleware from '@redux-saga/core';
 
 //thunk에서 라우터의 history 객체를 사용하려면 BrowserHistory 인스턴스를 직접 만들어서 적용해야 한다.
 const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware();
 
 // 미들웨어 여러개 적용 가능. logger는 가장 마지막에 와야함
 const store = createStore(
@@ -21,10 +23,14 @@ const store = createStore(
   composeWithDevTools(
     applyMiddleware(//redux-thunk의 withExtraArgument를 사용하면 thunk 함수에서 사전에 정해준 값들을 참조할 수 있다.
       ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware,
       logger
     )
   )
 );
+
+sagaMiddleware.run(rootSaga); // 루트 사가 실행
+// ⛔주의: 스토어를 생성한 다음에 위 코드를 작성해야함
 
 ReactDOM.render(
   <React.StrictMode>
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,18 +1,28 @@
+import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects'; //여기에 다양한 유틸 함수들이 있다.
+
 // 액션 타입
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
+const INCREASE_ASYNC = 'INCREASE_ASYNC';
+const DECREASE_ASYNC = 'DECREASE_ASYNC';
 
 // 액션 생성 함수
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-// thunk 함수 만들기
-// getState를 쓰지 않으면 굳이 파라미터로 받아올 필요X
-export const increaseAsync = () => dispatch => {
-    setTimeout(() => dispatch(increase()), 1000);
+function* increaseSaga() {
+    yield delay(1000); //1초 기다린다.
+    yield put(increase()); //put은 특정 액션을 디스패치 해준다.
 }
-export const decreaseAsync = () => dispatch => {
-    setTimeout(() => dispatch(decrease()), 1000);
+function* decreaseSaga() {
+    yield delay(1000);
+    yield put(decrease());
+}
+export function* counterSaga() { // 다른 곳에서 사용하므로 export
+    yield takeEvery(INCREASE_ASYNC, increaseSaga)//모든 INCREASE_ASYNC 액션 처리
+    yield takeLatest(DECREASE_ASYNC, decreaseSaga)//가장 마지막으로 디스패치된 DECREASE_ASYNC 액션만 처리
 }
 
 // 초기값 (상태가 객체가 아니라 그냥 숫자여도 상관X)
