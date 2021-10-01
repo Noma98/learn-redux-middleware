91766690e9f7763cb2def81673d8f8725c9d62e2
Noma <wiostz98kr@gmail.com>
2021-10-01 10:31:50 +0900

    Make posts module use redux-saga

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,10 @@
 import { all } from '@redux-saga/core/effects';
 import { combineReducers } from 'redux';
 import counter, { counterSaga } from './counter';
-import posts from './posts';
+import posts, { postsSaga } from './posts';
 
 const rootReducer = combineReducers({ counter, posts });
 export function* rootSaga() {
-    yield all([counterSaga()]); //all은 배열 안의 여러 사가를 동시에 실행시켜 준다.
+    yield all([counterSaga(), postsSaga()]); //all은 배열 안의 여러 사가를 동시에 실행시켜 준다.
 }
 export default rootReducer;
